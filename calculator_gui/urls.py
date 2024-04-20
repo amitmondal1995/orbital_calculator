2e375e8b92bc07cf54b86b35abaa2413f3ed2c92
Ayush Singh <ayushdevel1325@gmail.com>
2024-04-21 03:23:30 +0530

    sectionb: Add forms for part 2 and 3
    - Part 2 does not work
    
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- calculator_gui/urls.py
+++ calculator_gui/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("section_b", views.section_b, name="section b"),
+    path("section_b/1", views.section_b_1, name="section b 1"),
+    path("section_b/2", views.section_b_2, name="section b 1"),
+    path("section_b/3", views.section_b_3, name="section b 1"),
     path("hoffman", views.hoffman, name="Hoffman"),
     path("julian", views.julian, name="Julian"),
     path("lst", views.lst, name="LST"),
